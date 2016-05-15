1b97385a37c372d9bb122ac9dbd4751720885045
Kilian Sprotte <kilian.sprotte@gmail.com>
2016-05-15 09:36:12 +0000

    sbcl compile less verbose

--- .sbcl-disable-debugger.lisp
+++ .sbcl-disable-debugger.lisp
@@ -4,3 +4,8 @@
     (format *error-output* "ERROR of type ~S:~%~A~%" (type-of c) c)
     (sb-ext:exit :code 1)))
 (setq sb-ext:*invoke-debugger-hook* *debugger-hook*)
+
+(defmacro without-warnings (&body body) 
+  `(handler-bind ((warning #'muffle-warning)
+		 #+sbcl (sb-ext:compiler-note #'muffle-warning))
+    ,@body))
